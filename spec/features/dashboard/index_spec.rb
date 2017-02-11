c5c738fd34872b680af59f297ea2f1c1f61fa14c
Pablo Fonseca <pablofonseca777@gmail.com>
2017-02-11 14:13:04 -0200

    Remove smoke test

--- spec/features/dashboard/index_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-feature 'As an user I want to' do
-  scenario 'access the home page' do
-    visit root_path
-
-    expect(page).to have_content("Welcome to Be Vimmer")
-  end
-end
