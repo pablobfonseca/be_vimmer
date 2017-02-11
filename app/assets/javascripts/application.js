76ea1e6c063159fae1667e62032958023be9677d
Pablo Fonseca <pablofonseca777@gmail.com>
2017-02-11 14:26:08 -0200

    Add bower and bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
+//= require jquery-ujs
+//= require bootstrap/dist/js/bootstrap.min
 //= require turbolinks
 //= require_tree .
