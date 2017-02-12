7f81cb03b453d3e53adb83fe2773a2f6cff2ae74
Pablo Fonseca <pablofonseca777@gmail.com>
2017-02-12 15:46:28 -0200

    Add sad path to create command feature

--- app/models/command.rb
+++ app/models/command.rb
@@ -2,4 +2,6 @@ class Command < ApplicationRecord
   scope :normal_mode, -> { joins(:mode).merge(Mode.normal) }
   scope :visual_mode, -> { joins(:mode).merge(Mode.visual) }
   belongs_to :mode
+
+  validates_presence_of :mode_id, :command, :description
 end
--- spec/features/commands/create_spec.rb
+++ spec/features/commands/create_spec.rb
@@ -20,4 +20,17 @@ feature 'As a user I want to' do
     expect(page.current_path).to eq root_path
     expect(page).to have_content("A new command has been added")
   end
+
+  scenario 'see an error when trying to create an invalid command' do
+    visit root_path
+
+    within '.navbar' do
+      click_on 'Commands'
+      click_on 'New'
+    end
+
+    click_on "Create"
+
+    expect(page).to have_content(/can't be blank/)
+  end
 end
