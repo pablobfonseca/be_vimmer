7ad3d67924859399a3488bfc25cf266e9ee2966a
Pablo Fonseca <pablofonseca777@gmail.com>
2017-04-20 11:49:50 -0300

    Fix build

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
   def format_command command
+    return if command.nil?
+
     content_tag :div, class: 'command' do
       concat(content_tag :h4, "Command of the day: ")
       concat(content_tag :p, "#{command.command}: #{command.description}")
